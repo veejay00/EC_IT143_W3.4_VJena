d46be5b4d280ba74d668b250363c3cc9d98f07e4
Vincent Jena <vjena@byupathway.edu>
2025-02-11 07:26:58 +0200

    Add files via upload

---
+++ EC_IT143_W4.2_hello_world VJena.sql
@@ -0,0 +1,22 @@
+/*****************************************************************************************************************
+NAME:    My Script Name
+PURPOSE: My script purpose...
+
+MODIFICATION LOG:
+Ver      Date        Author        Description
+-----   ----------   -----------   -------------------------------------------------------------------------------
+1.0     10/02/2025   VJena       1. Built this script for EC IT440
+
+
+RUNTIME: 
+Xm Xs
+
+NOTES: 
+This is where I talk about what this script is, why I built it, and other stuff...
+ 
+******************************************************************************************************************/
+
+-- Q1: What should go here?
+-- A1: Question goes on the previous line, intoduction to the answer goes on this line...
+
+SELECT GETDATE() AS my_date;
